b85df7336225346788e65a1fe9062f0dc7301d35
gowthamreddy17 <saigowthamreddy1497@gmail.com>
2024-10-28 22:38:03 +0530

    api url change

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
 // export const API_URL = "http://localhost:8000"
 // https://backend-nodejs-suby.onrender.com
 // export const API_URL = "https://backend-nodejs-suby.onrender.com"
-export const API_URL = "https://foodvista-backend.onrender.com/"
+export const API_URL = "https://foodvista-backend.onrender.com"
